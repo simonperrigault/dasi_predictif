00912e676270130aa1b9713503912d7d1ac95266
Simon Perrigault <sperrigaul@insa-lyon.fr>
2024-05-06 17:56:49 +0200

    fin seance 1

--- IHM_dasi/src/main/java/modele/Action.java
+++ IHM_dasi/src/main/java/modele/Action.java
@@ -6,14 +6,20 @@
 package modele;
 
 import javax.servlet.http.HttpServletRequest;
+import metier.service.ServiceClient;
+import metier.service.ServiceEmploye;
 
 /**
  *
  * @author sperrigaul
  */
 public abstract class Action {
-    public Action() {
-        
+    protected ServiceClient serviceClient;
+    protected ServiceEmploye serviceEmploye;
+    
+    public Action(ServiceClient client, ServiceEmploye emplo) {
+        this.serviceClient = client;
+        this.serviceEmploye = emplo;
     }
     public abstract void execute(HttpServletRequest request);
 }
