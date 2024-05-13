c05e3ab4b006a25517ae732ed60972d66ab3ebd2
Cramara <cramara@users.noreply.github.com>
2024-05-13 17:37:06 +0200

    login js

--- IHM_dasi/src/main/webapp/scripts/login.js
+++ IHM_dasi/src/main/webapp/scripts/login.js
@@ -1,7 +1,6 @@
 
 var API_URL = 'http://localhost:8080/IHM_dasi/ActionServlet';
 
-<<<<<<< Updated upstream
 $(document).ready(() => {
     $("#boutonConnexion").on("click", () => {
         // Get the email and password from the form using jQuery
@@ -16,19 +15,6 @@ $(document).ready(() => {
                 mdp: mdp
             },
             dataType: 'json'
-=======
-function ConnectionClient() {
-    // Get the email and password from the form using jQuery
-    const email = $('#email').val();
-    const password = $('#mdp').val();
-    // Make a POST request to the API URL
-    $.post(API_URL, { todo : 'connecterClient', email: email, mdp: mdp })
-        .done(function(response) {
-            // Handle the successful response
-            console.log(response);
-            // Redirect to the desired page
-            //window.location.href = 'dashboard.html';
->>>>>>> Stashed changes
         })
                 .done((res) => {
                     console.log(res);
@@ -39,10 +25,5 @@ function ConnectionClient() {
                 });
     });
 
-<<<<<<< Updated upstream
 }
 );
-=======
-
-}
->>>>>>> Stashed changes
